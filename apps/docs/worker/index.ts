5b2112c2187854cf0fb00ca7ce32d1f0754dfeb1
ice breaker <1324318532@qq.com>
2026-09-01 14:18:59 +0800

    fix(docs): use DNS redirect for legacy domain

--- apps/docs/worker/index.ts
+++ apps/docs/worker/index.ts
@@ -12,12 +12,6 @@ interface WorkerHandler {
 
 export default {
   fetch(request: Request, env: Env): Promise<Response> {
-    const url = new URL(request.url)
-    if (url.hostname === 'sqlite.icebreaker.top') {
-      url.hostname = 'sqlite.weapp.dev'
-      return Promise.resolve(Response.redirect(url.toString(), 301))
-    }
-
     return env.ASSETS.fetch(request)
   },
 } satisfies WorkerHandler
